cb68c9ae834aab67a8b0395cd482f80c45b5e5b4
David Gipe <dwgipe@gmail.com>
2018-03-20 19:42:01 -0400

    Upgrades to ARM processor

--- SGSensor-Arduino.ino
+++ SGSensor-Arduino.ino
@@ -1,24 +1,19 @@
 /*
 * Ultrasonic Sensor - ECE4810
 *
-* Bench Test - V 2.0
+* Bench Test - V 2.1
 * 
 * David Gipe
 */
 
-//SD Card
-#include <SD.h>
-#include <SPI.h>
 
 #include <PulseInOne.h> //Sets to listen on INT1
-File dataFile;
-
 
 //Variables
   //Pins
   const int trigPin = 12; //Ultrasonic trigger
-  const int csPin = 53; //SD Card pin
-  const int tempPin = 1; //LM35 input
+  const int tempPin = 2; //LM35 input
+  const int volPin = A1; //Battery Monitoring
 
   //Specific Gravity
   int durationRaw;
@@ -32,25 +27,22 @@ File dataFile;
   float tempCel;
   float tempFar;
 
-  //Log Counters
-  int logCount = 0;
-  int logNum = 1;
-  
+  //Battery Voltage
+  float volActual;
+
 void setup() {
 
-  Serial.begin(9600);
+  SerialUSB.begin(9600);
 
   pinMode(trigPin, OUTPUT);
   digitalWrite(trigPin, LOW);
 
   PulseInOne::setup(pingReturn);
 
-  SD.begin();
-  pinMode(csPin, OUTPUT);
 }
 
 void loop() {
-
+ // SerialUSB.println("Startup Initiated");
   unsigned long time = millis();
   unsigned long dt   = time - lastTime;
   lastTime = time;
@@ -63,30 +55,14 @@ void loop() {
 
   tempRaw = analogRead(tempPin);
 
-//Calculations
+//Calculations - will soon be offloaded to app to compute
  //Temp
  tempCel = (( tempRaw/1024.0)*5000)/10;  //Obtained from internet, standard for LM35
  tempFar = (tempCel*9)/5 + 32; //Standard C to F conversion
-    
-if (logCount == 27280) //Only write to SD card every 10min
-{
- dataFile = SD.open("data.txt", FILE_WRITE);
- dataFile.println(logNum);
- dataFile.print("Raw delay time: ");
- dataFile.println(durationRaw);
- dataFile.print("Specific Gravity: ");
- dataFile.println(specificGravity, 3);
- dataFile.print("Temperature: ");
- dataFile.println(tempFar, 3);
- dataFile.println();
- dataFile.close(); 
 
-  logCount = 0; //reset "timer"
-  logNum++;
-  }
-
-
-logCount++;
+ //Voltage
+ volActual = analogRead(volPin) * (5.0 / 1023.0);
+   
 }
 
 
@@ -103,14 +79,16 @@ void ping(){
 void pingReturn(unsigned long duration) {
 
   durationRaw = duration;
-  specificGravity = .0003333*duration+.645;  //Equation from Y-intercept of two readings
+  specificGravity = .0003333*duration+.645;  //Equation from Y-intercept of two readings, arbirtrary
 
-  Serial.print("Raw: ");
-  Serial.print(duration);
-  Serial.print(" us, est. SG: ");
-  Serial.print(specificGravity, 3);
-  Serial.print(" Temp: ");
-  Serial.print(tempFar, 1);
-  Serial.println(" F");  
+  SerialUSB.print("Raw: ");
+  SerialUSB.print(duration);
+  SerialUSB.print(" us, est. SG: ");
+  SerialUSB.print(specificGravity, 3);
+  SerialUSB.print(" Temp: ");
+  SerialUSB.print(tempFar, 1);
+  SerialUSB.print(" F ");
+  SerialUSB.print(volActual);
+  SerialUSB.println(" V");   
 }
 
